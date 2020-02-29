9187b51fb34060e882d8c8befa9f35fd3eb68658
ana paula <anapaulamak@gmail.com>
2020-02-29 13:16:17 -0300

    Add context api

--- src/components/Departments.js
+++ src/components/Departments.js
@@ -1,35 +1,24 @@
 
-import React, { Component, Fragment } from 'react'
- 
-import Loading from './Loading'
- 
+import React, { Fragment } from 'react'
 import { getDepartments } from '../service/api'
- 
-class Departments extends Component {
- 
-    state = {
-        loading: false
-    }
- 
-    getDepartments = async () => {
-        this.setState({ loading: true })
+
+function Departments(props){
+    const handleGetDepartments = async () => {
+        const {showLoading, hideLoading} = props
+        showLoading('Carregando departamentos');
+
         const response = await getDepartments().then(response => {
-            this.setState({ loading: false })
+            hideLoading();
             return response
         })
         console.log({ response })
     }
  
-    render() {
-        const { loading } = this.state
- 
-        return (
-            <Fragment>
-                <button onClick={this.getDepartments}>Buscar departamentos</button>
-                <Loading loading={loading} message='Carregando departamentos...' />
-            </Fragment>
-        )
-    }
+    return (
+        <Fragment>
+            <button onClick={handleGetDepartments}>Buscar departamentos</button>
+        </Fragment>
+    )
 }
  
 export default Departments
